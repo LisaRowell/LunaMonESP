fc720a2c576d0b92a52ee7daf96c978010f1da53
Lisa Rowell <lisa.a.rowell@me.com>
2023-12-20 14:31:00 -0800

    Straightened out licensing information.

--- components/NMEA/NMEATime.cpp
+++ components/NMEA/NMEATime.cpp
@@ -1,5 +1,5 @@
 /*
- * This file is part of LunaMon (https://github.com/LisaRowell/LunaMon)
+ * This file is part of LunaMon (https://github.com/LisaRowell/LunaMonESP)
  * Copyright (C) 2021-2023 Lisa Rowell
  *
  * This program is free software: you can redistribute it and/or modify
